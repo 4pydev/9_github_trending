2ac898aac3d1fab1f3dd5284e6568e3d8e382ed7
4pydev <4pydev@gmail.com>
2017-11-19 21:39:31 +0300

    Changed func(get-open_issues_amount)

--- github_trending.py
+++ github_trending.py
@@ -16,18 +16,28 @@ def get_trending_repositories(top_size):
     return repos_list
 
 
-def get_open_issues_amount(repo):
-    return repo['open_issues']
+def get_open_issues_amount(repo_owner, repo_name):
+    github_issues_url = "https://api.github.com/repos" + "/" + \
+                         repo_owner + "/" + repo_name + "/issues"
+    search_params = {
+        'state': 'open'
+    }
+    response = requests.get(github_issues_url, search_params)
+
+    return len(response.json())
 
 
 if __name__ == '__main__':
+
     top_size = 20
     trending_repositories = get_trending_repositories(top_size)
     for repo in trending_repositories:
+        repo_open_issues = get_open_issues_amount(repo['owner']['login'],
+                                                  repo['name'])
         print('-------------------------------------')
         print("Repository owner: {owner}\nRepository name: {name}\nURL: {repo_url}"
-              "\nOpen issuues: {issues}"
+              "\nOpen issues: {issues}"
             .format(owner=repo['owner']['login'],
                     name=repo['name'],
                     repo_url=repo['owner']['html_url']+'/'+repo['name'],
-                    issues=get_open_issues_amount(repo)))
+                    issues=repo_open_issues))
