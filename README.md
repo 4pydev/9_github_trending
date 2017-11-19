55e2cd7e4d32f7a12218752db1850c8627adb2b2
4pydev <4pydev@gmail.com>
2017-11-19 20:00:12 +0300

    Updated .gitignore, README.md and github_trending.py

--- README.md
+++ README.md
@@ -1,7 +1,25 @@
 # Github Trends
 
-[TODO. There will be project description]
+Скрипт ищет 20 репозиториев, созданных за последнюю неделю и набравших наибольшее количество звёзд. 
 
+Для каждого из этих репозиториев в консоль выводится ссылка и количество открытых issues.
+
+# Usage
+
+Для использования необходим Python 3.5 с установленным модулем requests.
+
+Пример запуска и вывода скрипта:
+```bash
+$ python3 github_trending.py
+
+-------------------------------------
+Repository owner: google
+Repository name: puffs
+URL: https://github.com/google/puffs
+Open issuues: 0
+
+...
+``` 
 # Project Goals
 
 The code is written for educational purposes. Training course for web-developers - [DEVMAN.org](https://devman.org)
--- github_trending.py
+++ github_trending.py
@@ -2,11 +2,32 @@ import requests
 
 
 def get_trending_repositories(top_size):
-    pass
+    github_url = "https://api.github.com/search/repositories"
+    search_params = {
+        'q': 'created:>2017-11-12',
+        'page': '1',
+        'per_page': str(top_size),
+        'sort': 'stars',
+        'order': 'desc'
+    }
+    response = requests.get(github_url, search_params)
+    repos_list = response.json()['items']
+
+    return repos_list
 
 
-def get_open_issues_amount(repo_owner, repo_name):
-    pass
+def get_open_issues_amount(repo):
+    return repo['open_issues']
+
 
 if __name__ == '__main__':
-    pass
+    top_size = 20
+    trending_repositories = get_trending_repositories(top_size)
+    for repo in trending_repositories:
+        print('-------------------------------------')
+        print("Repository owner: {owner}\nRepository name: {name}\nURL: {repo_url}"
+              "\nOpen issuues: {issues}"
+            .format(owner=repo['owner']['login'],
+                    name=repo['name'],
+                    repo_url=repo['owner']['html_url']+'/'+repo['name'],
+                    issues=get_open_issues_amount(repo)))
